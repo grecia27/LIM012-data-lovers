45c846bee6ade8461bd608f15997691ad64db203
grecia.27 <greciarfr@hotmail.com>
2020-03-26 10:26:32 -0500

    despliegue de menu de busqueda, actualizacion de css de pokemones

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import data from './data/pokemon/pokemon.js';
 const stackOfPokemons = document.getElementById('stack');
 
 const showAllPokemon = (arr) => {
-arr.forEach((obj) => {
-const pokemonCard = document.createElement('div');
-pokemonCard.classList.add('cards')
-pokemonCard.innerHTML=`
+  arr.forEach((obj) => {
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('cards');
+    pokemonCard.innerHTML = `
                 <p>${obj.num}</p>
                 <div>
                     <img src="${obj.img}" alt="">
@@ -14,11 +14,11 @@ pokemonCard.innerHTML=`
                 <p>${obj.name}</p>
                 <p>${obj.type}</p>
                 `;
-stackOfPokemons.appendChild(pokemonCard);
-});
+    stackOfPokemons.appendChild(pokemonCard);
+  });
 };
-showAllPokemon(data.pokemon); 
+
+showAllPokemon(data.pokemon);
 
 
-
-//templates, backticks comillas invertidas para concatenar en JS
+// 'templates, backticks comillas invertidas para concatenar en JS'//
