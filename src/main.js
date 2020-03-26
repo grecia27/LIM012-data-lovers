68b293bf1c97dee2221704f70bf923c10b967f69
Elián Tapia Santa-Gadea <Elianetapia@users.noreply.github.com>
2020-03-26 10:23:47 -0500

    despliegue de menus de busqueda, ccs de cards de pokemon

--- src/main.js
+++ src/main.js
@@ -3,22 +3,19 @@ import data from './data/pokemon/pokemon.js';
 const stackOfPokemons = document.getElementById('stack');
 
 const showAllPokemon = (arr) => {
-arr.forEach((obj) => {
-const pokemonCard = document.createElement('div');
-pokemonCard.classList.add('cards')
-pokemonCard.innerHTML=`
+  arr.forEach((obj) => {
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('cards');
+    pokemonCard.innerHTML = `
                 <p>${obj.num}</p>
                 <div>
                     <img src="${obj.img}" alt="">
                 </div>
-                <p>${obj.name}</p>
+                <p><strong>${obj.name}</strong></p>
                 <p>${obj.type}</p>
                 `;
-stackOfPokemons.appendChild(pokemonCard);
-});
+    stackOfPokemons.appendChild(pokemonCard);
+  });
 };
-showAllPokemon(data.pokemon); 
-
-
-
-//templates, backticks comillas invertidas para concatenar en JS
+showAllPokemon(data.pokemon);
+// templates, backticks comillas invertidas para concatenar en JS
