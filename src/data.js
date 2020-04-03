6e2b845a4a11bc140bf03a3bb49bd42a78b286b1
Elián Tapia Santa-Gadea <Elianetapia@users.noreply.github.com>
2020-04-03 09:54:42 -0500

    actualizando

--- src/data.js
+++ src/data.js
@@ -1,4 +1,19 @@
-export const searchtext = (data, property, condition) =>{
-  const searchPkm = data.filter(element => (element[ property ]) . indexOf(condition) !==-1);
-  return searchPkm
-} ;
+export const searchtext = (data, property, condition) => {
+  const searchPkm = data.filter(element => (element[property]).indexOf(condition) !== -1);
+  return searchPkm;
+};
+
+// creamos una variable y lo igualamos a dos propiedades del dom, donde el resultado se guarda
+export const searchText = (data, findByName) => {
+  const result = [];
+  // forEach ejecuta una funcion proporcionada una vez para cada elemento
+  data.forEach((eachPokemon) => {
+    const namePokemon = eachPokemon.name;
+    const lengthtext = findByName.length;
+    if (namePokemon.slice(0, lengthtext) === findByName) {
+      result.push(eachPokemon);
+    }
+  });
+  return result;
+};
+console.log(searchText);
